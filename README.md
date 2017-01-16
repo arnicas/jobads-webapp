173ceda443e3b2d96c43a27eef2683c0d511452e
thomas <thomas.pucci@enfaia.com>
2017-01-16 11:43:30 +0100

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # jobads-webapp
 Job Ads Text Mining Project Web app interface.
+
+## Clone this repo
+
+Use the command `git clone https://github.com/tpucci/jobads-webapp.git`.
+
+## Wiki
+
+A wiki is provided : check it [here](https://github.com/tpucci/jobads-webapp/wiki).
+
+## Requirements
+
+This project requires a NodeJS server `^6.9.2` and NPM `^3.10.9`.
+
+## Development
+
+### React app
+
+You can build the React development app running `gulp watch`. Gulp watcher will refresh your local `public` directory every time you make a change.
+
+### Backend
+
+You need to restart the NodeJS Express server every time you make a change on the api source file :
+
+```javascript
+^C
+npm start
+```
+
+## Build and deploy
+
+You can build the React app running `gulp build`.
+Flow is optimized by automatically deploying to staging using GitHub integratiopn on Heroku.
+"For example whenever the master branch is updated on GitHub and continuous integration (CI) tests pass, staging can be auto-deployed."[Learn more](https://devcenter.heroku.com/articles/pipelines#github-sync)
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
+var babelify = require('babelify');
+var sass = require('gulp-sass');
+
+// Paths and filenames
+var path = {
+    HTML: 'src/*.html',
+    SCSS: 'src/sass/**/*.css',
+    IMAGES: 'src/images/**/*',
+    VENDORS: 'src/vendors/**/*',
+    MINIFIED_OUT: 'build.min.js',
+    OUT: 'build.js',
+    DEST: 'public',
+    DEST_BUILD: 'public/src',
+    DEST_SRC: 'public/src',
+    DEST_VENDORS: 'public/vendors',
+    DEST_IMAGES: 'public/images',
+    ENTRY_POINT: './src/js/app.js'
+};
+
+// Copy assets
+gulp.task('copy', function(){
+    gulp.src(path.HTML)
+        .pipe(gulp.dest(path.DEST));
+    gulp.src(path.VENDORS)
+        .pipe(gulp.dest(path.DEST_VENDORS));
+    gulp.src(path.IMAGES)
+        .pipe(gulp.dest(path.DEST_IMAGES));
+});
+
+// Build css style
+gulp.task('sass', function () {
+  return gulp.src(path.SCSS)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(path.DEST));
+});
+
+// Watch for any change
+gulp.task('watch', function() {
+    gulp.watch(path.HTML, ['copy']);
+    gulp.watch(path.SCSS, ['sass']);
+    gulp.watch(path.VENDORS, ['copy']);
+    var watcher  = watchify(browserify({
+        entries: [path.ENTRY_POINT],
+        transform: [babelify, reactify],
+        debug: true,
+        cache: {}, packageCache: {}, fullPaths: true
+    }));
+    return watcher.on('update', function () {
+        watcher.bundle()
+            .on('error', function (err) {
+                console.log(err.toString());
+                this.emit("end");
+            })
+            .pipe(source(path.OUT))
+            .pipe(gulp.dest(path.DEST_SRC))
+        console.log('Updated');
+    })
+        .bundle()
+        .pipe(source(path.OUT))
+        .pipe(gulp.dest(path.DEST_SRC));
+});
+
+// Build the React App
+gulp.task('build', function(){
+    browserify({
+        entries: [path.ENTRY_POINT],
+        transform: [babelify, reactify],
+        debug: false
+    })
+        .bundle()
+        .pipe(source(path.MINIFIED_OUT))
+        .pipe(streamify(uglify(path.MINIFIED_OUT)))
+        .pipe(gulp.dest(path.DEST_BUILD));
+});
+
+// Include the build script in HTML main page 
+gulp.task('replaceHTML', function(){
+    gulp.src(path.HTML)
+        .pipe(htmlreplace({
+            'js': 'build/' + path.MINIFIED_OUT
+        }))
+        .pipe(gulp.dest(path.DEST));
+});
+
+// Production : replace HTML and build the app
+gulp.task('production', ['replaceHTML', 'build']);
+
+// Default : watch
+gulp.task('default', ['watch']);
