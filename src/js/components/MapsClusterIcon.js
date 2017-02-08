7dae0654dc0fff1cd47e10891fd7073f11718abb
thomas <thomas.pucci@enfaia.com>
2017-02-08 19:28:38 +0100

    Map Filter

---
+++ src/js/components/MapsClusterIcon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SvgIcon from 'material-ui/SvgIcon';
+
+const iconStyle = {
+  viewBox: "0 0 24 24",
+};
+
+const MapsPlaceIcon = (props) => (
+  <SvgIcon {...props} viewBox={iconStyle.viewBox}>
+      <defs>
+          <linearGradient id="MyGradient2">
+              <stop offset="0%" stopColor="#b64be8" />
+              <stop offset="100%" stopColor="#faa159" />
+          </linearGradient>
+      </defs>
+        <path d="M 12,2 C 8.13,2 5,5.13 5,9 c 0,5.25 7,13 7,13 0,0 7,-7.75 7,-13 0,-3.87 -3.13,-7 -7,-7 z"></path>
+    </SvgIcon>
+);
+
+export default MapsPlaceIcon;
